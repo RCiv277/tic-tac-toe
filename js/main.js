88cbc1d17a471bed0545269c3f75fbd102a75d3e
Andrew Murray <darbyguitars@gmail.com>
2019-10-25 13:57:16 -0500

    Framework update number 1 i expect 2 more

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Constants                    
+
+
+
+
+
+// Global Variables: eso es un pecado
+
+
+
+
+// Objects Main Array: Using an alignment access for positional reference IE:
+//   LG  NG  CG
+//   LN  TN  CN
+//   LE  NE  CE
+
+const pstnArryOg = ['LG' , 'NG' , 'CG' , 'LN' , 'TN' , 'CN' , 'LE' , 'NE' , 'CE']
+function startGame(assnPlayer){    // ASSIGN A PLAYER VALUE PLX
+ 
+   let newPstnArry = pstnArryOg.map(element => {   //Resets The Board/Assigns Boardstate
+        return {
+        status: 'Inert',
+        player: null,
+        element
+        }
+    }
+    )
+};
+
+
+
+
+
+
+
+
+//Function related to starting the game/deciding turn order/game logic
+
+
+
+
+
+
+//Functions related to dom/html
+
+
+
+
+
+
+
+
+
+
+
