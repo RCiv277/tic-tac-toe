ba5c39cafd9430e2003468b5d68a27c2be32337c
Andrew Murray <darbyguitars@gmail.com>
2019-10-27 19:32:47 -0500

    created the css/html, began hooking the framework functions together

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function winPossibP1() {
 (newPstnArry[1].PlayerOne && newPstnArry[4].PlayerOne && newPstnArry[7].PlayerOne) ||
 (newPstnArry[2].PlayerOne && newPstnArry[5].PlayerOne && newPstnArry[8].PlayerOne) ||
 (newPstnArry[0].PlayerOne && newPstnArry[4].PlayerOne && newPstnArry[8].PlayerOne) ||
-(newPstnArry[6].PlayerOne && newPstnArry[4].PlayerOne && newPstnArry[2].PlayerOne) ? true : false;}
+(newPstnArry[6].PlayerOne && newPstnArry[4].PlayerOne && newPstnArry[2].PlayerOne) ? (true , playerOneWin = true) : false;}
 
 function winPossibP2() {
 (newPstnArry[0].PlayerTwo && newPstnArry[1].PlayerTwo && newPstnArry[3].PlayerTwo) ||
@@ -19,7 +19,7 @@ function winPossibP2() {
 (newPstnArry[1].PlayerTwo && newPstnArry[4].PlayerTwo && newPstnArry[7].PlayerTwo) ||
 (newPstnArry[2].PlayerTwo && newPstnArry[5].PlayerTwo && newPstnArry[8].PlayerTwo) ||
 (newPstnArry[0].PlayerTwo && newPstnArry[4].PlayerTwo && newPstnArry[8].PlayerTwo) ||
-(newPstnArry[6].PlayerTwo && newPstnArry[4].PlayerTwo && newPstnArry[2].PlayerTwo) ? true : false;}
+(newPstnArry[6].PlayerTwo && newPstnArry[4].PlayerTwo && newPstnArry[2].PlayerTwo) ? (true , playerTwoWin = true) : false;}
 
 function didSomeoneWin(){winPossibP1() || winPossibP2() ? true: false ;}
 
@@ -32,31 +32,33 @@ function didSomeoneWin(){winPossibP1() || winPossibP2() ? true: false ;}
 
 
 
-const pstnArryOg = ['LG' , 'NG' , 'CG' , 'LN' , 'TN' , 'CN' , 'LE' , 'NE' , 'CE']
+var pstnArryOg = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8']
 
 
 // Global Variables: eso es un pecado
 
 newPstnArry = []
-turnOrder = true                // True = Player One! False = Player Two!
+var turnOrder = true                                                                           // True = Player One! False = Player Two!
+var playerOneWin = false
+var playerTwoWin = false  
 
-// Objects Main Array: Using an alignment access for positional reference IE:
+// Objects Main Array: Using an alignment access(outdated info efficency wise; i did a good) for positional reference IE:
 //   LG (0) NG (1) CG (2)
 //   LN (3) TN (4) CN (5)
 //   LE (6) NE (7) CE (8)
 
-function startGame(assnPlayer){    // ASSIGN A PLAYER VALUE PLX
-    let playerOneWin = false
-    let playerTwoWin = false    
+function startGame(assnPlayer){    
+    playerOneWin = false
+    playerTwoWin = false    
+    turnOrder = true
 
 
-
-   newPstnArry = pstnArryOg.map(element => {   //Resets The Board/Assigns Boardstate
+   newPstnArry = pstnArryOg.map(element => {                                                    //Resets The Board/Assigns Boardstate
         return {
         Status: 'Inert',
         PlayerOne: false,
         PlayerTwo: false,
-        element
+        item: document.getElementById(`${element}`),                                    //much much much faith
         }
     }
     )
@@ -69,19 +71,26 @@ function startGame(assnPlayer){    // ASSIGN A PLAYER VALUE PLX
 
 
 
+
 //Function related to starting the game/deciding turn order/game logic
 
 function winConditions(){
-    didSomeoneWin() ? true: false; //if true display winner prompt  , if false carry on
+    didSomeoneWin() ? true:
+    false; //if true display winner prompt  , if false change turn prompt and image
+}
+function reactToClick(){
+    event.target = /* display picture */ (turnOrder ? /* shaz pic */ 0: 0 /*andrew pic */;)
+    winConditions()
+    turnOrder? turnOrder = false: turnOrder = true;
 }
 
 
 
 
 //Functions related to dom/html/event listeners
+//event target
 
-
-
+.addeventlistener('click' ,reactToClick)
 
 
 //Notes/ Bugs
